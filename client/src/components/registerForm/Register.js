b4aefd84cea576f9bdea82372bd79deae1358d75
dumbleedore <lucas.pari@icloud.com>
2020-10-01 14:29:04 -0300

    deixando formregister mais enxuto

--- client/src/components/registerForm/Register.js
+++ client/src/components/registerForm/Register.js
@@ -2,24 +2,22 @@ import React from 'react';
 import './registerstyle.css';
 const Register = () => {
   const [cond, setCond] = React.useState(false);
-  function handleClick() {
+  const [form, setForm] = React.useState({
+    nome: '',
+    username: '',
+    password: '',
+    email: '',
+  });
+
+  function handleSubmit(event) {
     // POST request using fetch()
-    const user = document.getElementById('login').value;
-    const password = document.getElementById('password').value;
-    const email = document.getElementById('email').value;
-    const name = document.getElementById('name').value;
-    console.log(user, password);
+    event.preventDefault();
     fetch('http://localhost:5000/api//users/add', {
       // Adding method type
       method: 'POST',
 
       // Adding body or contents to send
-      body: JSON.stringify({
-        username: user,
-        password: password,
-        email: email,
-        name: name,
-      }),
+      body: JSON.stringify(form),
 
       // Adding headers to the request
       headers: {
@@ -31,56 +29,65 @@ const Register = () => {
 
       // Displaying results to console
       .then((json) => console.log(json));
-    document.getElementById('login').value = null;
-    document.getElementById('password').value = null;
-    document.getElementById('email').value = null;
-    document.getElementById('name').value = null;
     setCond(true);
     setTimeout(() => {
       setCond(false);
     }, 1000);
+  }
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
   }
   return (
     <div>
       <div className="wrapper fadeInDown">
         <div id="formContent">
           {cond && (
-            <div class="alert alert-success" role="alert">
+            <div className="alert alert-success" role="alert">
               Cadastro feito com sucesso!
             </div>
           )}
-          <div className="fadeIn first"></div>
-          <input
-            type="text"
-            id="name"
-            className="fadeIn second"
-            placeholder="Nome"
-          />
+          <form onSubmit={handleSubmit}>
+            <div className="fadeIn first"></div>
+            <input
+              type="text"
+              id="nome"
+              className="fadeIn second"
+              placeholder="Nome"
+              value={form.nome}
+              required
+              onChange={handleChange}
+            />
 
-          <input
-            type="text"
-            id="login"
-            className="fadeIn second"
-            placeholder="Username"
-          />
-          <input
-            type="password"
-            id="password"
-            className="fadeIn third"
-            placeholder="Password"
-          />
-          <input
-            type="email"
-            id="email"
-            className="fadeIn third"
-            placeholder="Email"
-          />
-          <input
-            type="submit"
-            className="fadeIn fourth"
-            value="Register"
-            onClick={handleClick}
-          />
+            <input
+              type="text"
+              id="username"
+              className="fadeIn second"
+              placeholder="Username"
+              value={form.username}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="password"
+              id="password"
+              className="fadeIn third"
+              placeholder="Password"
+              value={form.password}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="email"
+              id="email"
+              className="fadeIn third"
+              placeholder="Email"
+              value={form.email}
+              onChange={handleChange}
+              required
+            />
+            <input type="submit" className="fadeIn fourth" value="Register" />
+          </form>
         </div>
       </div>
     </div>
